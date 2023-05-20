4841c097b60b11689f7ea4054aec07baa19114c7
ANGELOANTU7 <angeloantu@gmail.com>
2023-05-20 06:39:20 +0530

    narr

--- Python_Code/Narrator.py
+++ Python_Code/Narrator.py
@@ -5,21 +5,35 @@ import os
 from urllib.parse import urlparse, urljoin
 
 
+
 openai.api_key = "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"
-# Function to search for an image related to a given query
-# Function to search for an image related to a given query
-# Function to search for an image related to a given query
+
+# Process each paragraph and search for related images
+def process_paragraphs(paragraphs):
+    # Create the "images" directory if it doesn't exist
+    if not os.path.exists("images"):
+        os.makedirs("images")
+    for query in paragraphs:
+        image_url = search_image(query)
+        if image_url:
+            # Download the image and save it locally
+            response = requests.get(image_url)
+            if response.status_code == 200:
+                with open(f"images/image_{query}.jpg", 'wb') as f:
+                    f.write(response.content)
+                    print(f"Image_{query}.jpg saved.")
+
 def search_image(query):
-    url = f"https://www.google.com/search?q={query}&tbm=isch"
+    url = f"https://www.google.com/search?q={query}&tbm=isch&tbs=isz:l"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
-    image_element = soup.find('img')
+    image_elements = soup.find_all('img')
     image_url = None
-    if image_element:
-        image_url = image_element.get('src')
+    if len(image_elements) > 1:
+        image_url = image_elements[1].get('src')
         if image_url:
             parsed_url = urlparse(image_url)
             if not parsed_url.scheme:
@@ -27,22 +41,7 @@ def search_image(query):
                 image_url = urljoin(base_url, image_url)
     return image_url
 
-# Process each paragraph and search for related images
-def process_paragraphs(paragraphs):
-    # Create the "images" directory if it doesn't exist
-    if not os.path.exists("images"):
-        os.makedirs("images")
-    for i, paragraph in enumerate(paragraphs):
-        query = paragraph.strip()
-        image_url = search_image(query)
-        if image_url:
-            # Download the image and save it locally
-            response = requests.get(image_url)
-            if response.status_code == 200:
-                with open(f"images/image_{i}.jpg", 'wb') as f:
-                    f.write(response.content)
-                    print(f"Image_{i}.jpg saved.")
-                    
+
 def extract_text_from_file(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
@@ -59,7 +58,7 @@ def extract_important_topics(questions):
         messages=[
             {
                 "role": "user",
-                "content": f"You are a teacher , who will take a Topic to a student, i will provide you with basic summary of student's lecture note which may be bit incomplete , by you have to use it and interact with student and make sure he understand it fully, basically you have to narrate (everthing should be text):\n\n{text}\n\n"
+                "content": f"You are a teacher, who will take a Topic to a student. I will provide you with a basic summary of the student's lecture note, which may be a bit incomplete, but you have to use it and interact with the student and make sure they understand it fully. Basically, you have to narrate (everything should be text):\n\n{text}\n\n"
             }
         ]
     )
@@ -68,28 +67,59 @@ def extract_important_topics(questions):
     print(important_topics)
     return important_topics
 
-file_path = 'Files\generated_files\summarised_notes\module1_summarized.txt' 
+
+def extract_image_name(questions):
+    text = questions
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": f"create a word for each paragraph as a list\n\n{text}\n\n"
+            }
+        ]
+    )
+
+    important_topics = response.choices[0].message.content
+    print(important_topics)
+    return important_topics
+
+file_path = 'Files\generated_files\summarised_notes\module1_summarized.txt'
 extracted_text = extract_text_from_file(file_path)
 narrate = extract_important_topics(extracted_text)
 
-
 print(narrate)
-# Split the text into paragraphs
-paragraphs = [p.strip() for p in narrate.split('\n') if p.strip()]
+# Save topics to topics.txt
+with open('Files\generated_files\\narration.txt', 'w', encoding='utf-8') as file:
+    file.write(narrate)
+
+image_name=extract_image_name(narrate)
+image_list = image_name.splitlines()
 
 # Create a directory to store the images
 os.makedirs("images", exist_ok=True)
 os.chdir("images")
 
 # Process the paragraphs and search for related images
-process_paragraphs(paragraphs)
+print(image_list)
+process_paragraphs(image_list)
 
 # Return to the parent directory
 os.chdir("..")
 
 while(True):
-    text=input(" to exit enter 0 :")
-    if(text=="0"):
-        break
-    narrate = extract_important_topics(text)
-    print(narrate)
+    with open('Files\generated_files\\response.txt', 'r',encoding='utf-8') as file:
+        content = file.read()
+    if len(content)==0:
+        continue
+    else:
+        if content == "understood":
+            break
+        if content[0]==".":
+            narrate = extract_important_topics(content)
+            with open('Files\generated_files\\narration.txt', 'w', encoding='utf-8') as file:
+                file.write(narrate)
+            print(narrate)
+            with open('Files\generated_files\\narration.txt', 'w') as file:
+                # Truncate the file to remove its contents
+                file.truncate()
--- Python_Code/tempCodeRunnerFile.py
+++ Python_Code/tempCodeRunnerFile.py
@@ -1,38 +1,11 @@
-from transformers import pipeline
 
-# Load the summarization pipeline
-summarizer = pipeline("summarization")
+# Process the paragraphs and search for related images
+print(image_list)
+process_paragraphs(image_list)
 
-# Read the contents of the text file
-with open("Files\\notes_txt\module1.txt", "r", encoding='utf-8') as file:
-    text = file.read()
+# Return to the parent directory
+os.chdir("..")
 
-# Split the text into smaller chunks
-max_tokens_per_chunk = 2000
-max_words_in_summary = 2000000
-
-# Calculate the maximum number of chunks needed
-max_num_chunks = (max_words_in_summary // max_tokens_per_chunk) + 1
-print("\n chunks :"+str(max_num_chunks))
-
-maxlen=max_words_in_summary/max_num_chunks
-
-# Split the text into chunks
-chunks = [text[i:i + max_tokens_per_chunk] for i in range(0, len(text), max_tokens_per_chunk)]
-
-# Generate summaries for each chunk
-summaries = []
-for i, chunk in enumerate(chunks):
-    # Break the loop if the maximum number of chunks has been reached
-    if i >= max_num_chunks:
-        break
-    summary = summarizer(chunk, max_length=200, min_length=100, do_sample=False)
-    summaries.append(summary[0]['summary_text'])
-    print(summary[0]['summary_text'])
-
-# Combine the summaries into a single summary
-combined_summary = " ".join(summaries)
-
-# Print the combined summary
-print("Combined Summary:")
-print(combined_summary)
+while(True):
+    with open('Files\generated_files\\response.txt', 'r',encoding='utf-8') as file:
+        content = file.read()
